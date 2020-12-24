7756870494689adfcc85553254b8a3b6bbc4a232
Bob Briski <rbriski@gmail.com>
2020-12-24 11:50:10 -0800

    set up for prod/dev reqs.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 __version__ = '3.0.0'
 
 
-requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements', 'prod.txt')
 with open(requirements_path) as requirements_file:
     requirements = requirements_file.readlines()
 
